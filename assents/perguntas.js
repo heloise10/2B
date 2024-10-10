1aa23674d31794bbf12f49c61c5e060d65d754c6
heloise tavares <142838869+heloise10@users.noreply.github.com>
2024-10-10 14:08:15 -0300

    commit

---
+++ assents/perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    categoria,pergunta,resposta,
+)
+
+criaCartao(
+    'programação','o que é JavaScript', 'o JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'geografia', 'qual a capital da frança?' , ' a capital da frança é paris '
+)
+
+criaCartao(
+    'programação', 'o que é uma função?', 'uma função é um bloco de código que executa alguma tarefa'
+)
